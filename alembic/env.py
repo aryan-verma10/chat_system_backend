a2d284c8a831e1844de7f0b05f0f2a39a9e621b3
aryan-verma10 <dpsary9339@gmail.com>
2025-03-03 22:02:07 +0530

    [UPDATE] adding new migration files

--- alembic/env.py
+++ alembic/env.py
@@ -6,6 +6,9 @@ from sqlalchemy import pool
 from alembic import context
 from src.database import engine, Base, postgres_url
 
+# IMPORTANT
+from src.user.models import User
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -20,7 +23,7 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
-
+print("target_metadata->", target_metadata)
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
